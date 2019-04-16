ff27fc6fe5bac484af12968ff15f754f2741dc70
daisuke <dada.00131@google.com>
2019-04-16 16:17:52 +0900

    rubocop

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,6 @@
-require_relative 'boot'
+require_relative "boot"
 
-require 'rails/all'
+require "rails/all"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -21,13 +21,13 @@ module Qoota
       g.stylesheets false
       g.helper false
       g.test_framework :rspec,
-      fixtures: true,
-      fixture_replacement: :factory_bot,
-      view_specs: false,
-      routing_specs: false,
-      helper_specs: false,
-      controller_specs: false,
-      request_specs: true
+                       fixtures: true,
+                       fixture_replacement: :factory_bot,
+                       view_specs: false,
+                       routing_specs: false,
+                       helper_specs: false,
+                       controller_specs: false,
+                       request_specs: true
     end
     # config.api_only = true
     # config.middleware.use ActionDispatch::Flash
