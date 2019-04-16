2f588b0366b5eb37657f6d67c366532afedd1d83
daisuke <dada.00131@google.com>
2019-04-16 17:59:30 +0900

    modified article.rb

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,3 +1,7 @@
 class Article < ApplicationRecord
   belongs_to :user
+  has_many :article_tags, dependent: :destroy
+  has_many :tags, through: :article_tags
+  has_many :likes, dependent: :destroy
+  enum status: { draft: 0, published: 1, secret: 2 }
 end
