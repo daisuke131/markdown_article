13100be7b646054aaf397ee6d2df8fdd21d5885e
daisuke <dada.00131@google.com>
2019-04-16 18:00:22 +0900

    modified user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,9 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable, :confirmable
+         :recoverable, :rememberable, :validatable # , :confirmable
+  has_many :articles, dependent: :destroy
+  has_many :likes, dependent: :destroy
+  has_many :like_articles, through: :likes, source: :articles
+  has_many :communities, through: :user_communities
 end
