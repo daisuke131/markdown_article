19cece96292f869912f9d8ca98ffe1ff8c4fff97
daisuke <dada.00131@google.com>
2019-04-16 18:00:37 +0900

    modified routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
+  root "articles#index"
   devise_for :users
-  get "articles/index"
+  # get "articles/index"
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
