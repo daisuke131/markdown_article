203ec69d933a517b2f26c8922702c79a7da71dff
daisuke <dada.00131@google.com>
2019-04-16 20:59:41 +0900

    modified article.rb

--- app/models/article.rb
+++ app/models/article.rb
@@ -3,5 +3,8 @@ class Article < ApplicationRecord
   has_many :article_tags, dependent: :destroy
   has_many :tags, through: :article_tags
   has_many :likes, dependent: :destroy
+
+  validates :title, presence: true
+  validates :body, presence: true
   enum status: { draft: 0, published: 1, secret: 2 }
 end
