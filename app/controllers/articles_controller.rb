143cde180eb31bb2177659b83ecd5d5d7a9191ee
daisuke <dada.00131@google.com>
2019-04-17 17:10:49 +0900

    modified articles_controller

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,25 +3,35 @@ class ArticlesController < ApplicationController
   before_action :set_article, only: [:update, :destroy]
 
   def index
-    @articles = Article.published.left_joins(:user).select("articles.*, users.username").order("created_at desc")
+    @articles = Article.published.left_joins(:user).select("articles.*, users.id as user_id, users.username").order("created_at desc")
   end
 
   def show
     @article =
-      if current_user
-        current_user.articles.published.left_joins(:user).select("articles.*, users.username").find(params[:id])
+      if current_user.id == params[:id]
+
+        current_user.articles.left_joins(:user).select("articles.*, users.username").find(params[:id])
       else
         Article.published.left_joins(:user).select("articles.*, users.username").find(params[:id])
       end
-    end
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  def edit
+    @article = current_user.articles.find(params[:id])
   end
 
   def create
     current_user.articles.create!(article_params)
+    redirect_to root_path
   end
 
   def update
     @article.update!(article_params)
+    redirect_to root_path
   end
 
   def delete
@@ -29,11 +39,12 @@ class ArticlesController < ApplicationController
   end
 
   private
-  def set_article
-    @article = current_user.articles.find(params[:id])
-  end
 
-  def article_params
-    params.require(:article).permit(:title, :body, :status)
-  end
+    def set_article
+      @article = current_user.articles.find(params[:id])
+    end
+
+    def article_params
+      params.require(:article).permit(:title, :body, :status)
+    end
 end
