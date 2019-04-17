fb39b1d3a7eb2be551502f6281363fab032dedfc
daisuke <dada.00131@google.com>
2019-04-17 17:12:39 +0900

    modified nav

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root "articles#index"
   devise_for :users
+  get "users/:id" => "users#show", as: "user"
   resources :articles
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
