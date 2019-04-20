b231c81f43025405956d651c4732907e9847ea15
daisuke <dada.00131@google.com>
2019-04-20 17:40:52 +0900

    modified devise files

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,8 +1,5 @@
 class Article < ApplicationRecord
   belongs_to :user
-  has_many :article_tags, dependent: :destroy
-  has_many :tags, through: :article_tags
-  has_many :likes, dependent: :destroy
 
   validates :title, presence: true
   validates :body, presence: true
--- app/models/community.rb
+++
@@ -1,4 +0,0 @@
-class Community < ApplicationRecord
-  has_many :user_communities, dependent: :destroy
-  has_many :users, through: :user_communities
-end
